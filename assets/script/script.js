6b0d2d25d565568671dde076a112956656c800e2
Reuse-it <reuseit.gent@gmail.com>
2023-05-24 10:50:21 +0200

    adding data

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,81 @@
+//Contentful
 
+const spaceId = "e6aae04j966s";
+const environmentId = "master";
+const accessToken = "L67tgUtJIAByIcmxzJjZgoBY7O6d8zGG5EQ0hwhyEG8";
+
+const url = `https://cdn.contentful.com/spaces/${spaceId}/environments/${environmentId}/entries?access_token=${accessToken}`
+console.log(url);
+
+const sectionTag = document.querySelector("div.main-archive-container");
+
+// Fetch Data
+// fetching data for the archive page content
+
+const getData = function () {
+    return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        //store the assets images for the archive page
+        const assets = data.includes.Asset
+
+        //contentful data
+        return data.items.map( item => {
+
+            let imageUrl = "./assets/img/bloem.svg";
+            let originalUrl = "./assets/img/bloem.svg";
+            
+            const imageId = item.fields.reuseitObject.sys.id;
+            
+            const originalImageId = item.fields.original.sys.id;
+
+            const imageData = assets.find(asset => {
+                return asset.sys.id == imageId
+            })
+
+            const originalImageData = assets.find(asset => {
+                return asset.sys.id == originalImageId
+            })
+
+            if(imageData){
+                imageUrl = imageData.fields.file.url
+            }
+
+            if(originalImageData){
+                originalUrl = originalImageData.fields.file.url
+            }
+        
+            item.fields.reuseitObject = imageUrl
+            item.fields.original = originalUrl
+            return item.fields
+
+        })
+    })
+}
+
+
+// Run getData function on load
+
+getData().then(data => {
+    
+    sectionTag.innerHTML = ""
+
+    data.forEach(item => {
+        sectionTag.innerHTML = sectionTag.innerHTML + `
+        
+                <div class="col-sm-4 col-lg-4 reuseitObject-container">
+                    <img class="img-fluid img-thumbnail reuseitObject-image" src="${item.reuseitObject}">
+                    <div class="text-center">
+                        <p>${item.title}</p>
+                        <a class="archive-link" href="${item.original}" target="_blank">link to the original object</a>
+                        <a href="${item.reuseitObject}" download>
+                            <img class="mt-4" src="./assets/img/downloadIcon.svg" alt="download icon">
+                        </a>
+                    </div>
+                </div>
+        
+        `
+    });
+
+})
